00072b30cc6d10abd4712414669e4dcc1fd75e12
Reznichenko Sergey <sergiliocr@gmail.com>
2022-06-07 11:46:56 +0300

    feat: add cv sections

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function uniqueInOrder(it) {
+    var result = [];
+    var last;
+    for (var i = 0; i < it.length; i++) {
+        if (it[i] !== last) {
+            result.push(last = it[i]);
+        }
+    }
+    return result
+}
